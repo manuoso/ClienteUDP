9543cdaffac612bf53a002a36ca75f5e31c01962
Unknown <manuperezj@gmail.com>
2018-05-24 11:43:10 +0200

    update with log manager

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include "LogManager.h"
 
 struct datos{
         float x;
@@ -21,14 +22,16 @@ using boost::asio::ip::udp;
 
 int main(int argc, char* argv[]) {
 
+	LogManager::init("ReceivedClientUDP_" + std::to_string(time(NULL)));
+
 	try {
 		boost::asio::io_service io_service;
-		
-		std::cout << "Enter IP of server to connect: ";	
+	
+		LogManager::get()->status("Enter IP of server to connect: ", true);
 		std::string ipServerUDP;
 		std::cin >> ipServerUDP;
 
-		std::cout << "Enter PORT of server to connect: ";	
+		LogManager::get()->status("Enter PORT of server to connect: ", true);	
 		int portServerUDP;
 		std::cin >> portServerUDP;
 
@@ -40,6 +43,9 @@ int main(int argc, char* argv[]) {
 		boost::array<char, 1> send_buf = { { 0 } };
 		socket.send_to(boost::asio::buffer(send_buf), endpoint);
 
+		
+		LogManager::get()->status("Started to receive data", true);
+
 		while(true){
 			boost::array<char, sizeof(datos)> recv_buf;
 			udp::endpoint sender_endpoint;
@@ -48,14 +54,19 @@ int main(int argc, char* argv[]) {
 			datos data_recv;
 			memcpy(&data_recv, &recv_buf[0], sizeof(datos));
 			
-			std::cout << "Lenght received: " << len << std::endl;
+			std::cout << "length received: " << len << std::endl;
 			std::cout << "x: " << data_recv.x << ", y: " << data_recv.y << ", z: " << data_recv.z << ", timestamp: " << data_recv.timestamp << std::endl;
+
+			LogManager::get()->message(std::to_string(len), "LENGTH", false);
+			LogManager::get()->message(std::to_string(data_recv.x) + "," + std::to_string(data_recv.y) + "," + std::to_string(data_recv.z) + "," + std::to_string(data_recv.timestamp), "RECEIVED", false);
 		}
 	}
 	catch (std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+
+	LogManager::close();
 
 	return 0;
 }
